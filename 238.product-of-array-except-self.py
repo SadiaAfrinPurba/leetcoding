5317c9ac0107e785747422707d61799abd1f1cf9
SadiaAfrinPurba <purba0101@gmail.com>
2023-01-01 22:15:52 +0600

    Solve 238.product-of-array-except-self

--- 238.product-of-array-except-self.py
+++ 238.product-of-array-except-self.py
@@ -14,38 +14,21 @@ from typing import List
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
        
-        if all(num == 0 for num in nums):
-            return nums
-
-
         res = [1] * len(nums)
 
-        if 0 in nums:
-            copy_nums = copy.deepcopy(nums)
+        # calculate prefix product
+        prefix = 1
+        for idx in range(len(nums)):
+            res[idx] = prefix
+            prefix *= nums[idx]
 
-            for idx, num in enumerate(nums):
-                if num != 0:
-                    res[idx] = 0
-                else:
-                    copy_nums.remove(0)
-                    res[idx] = reduce(mul, copy_nums)
-            return res
-
-        res[0] = reduce(mul, nums[1:])
-        for idx in range(1, len(nums)):
-            res[idx] = (res[idx - 1] * nums[idx - 1]) // nums[idx]
+        postfix = 1
+        for idx in range(len(nums) -1, -1, -1):
+            res[idx] *= postfix
+            postfix *= nums[idx]
 
         return res
-        
-if __name__ == '__main__':
-    # Failing case-1: [0, 0]
-    # Failing case-2: [0, 4, 0]
-    # Learning: think about how to handle zero in computations 
-    # of product
 
-    res = Solution().productExceptSelf(nums=[0, 4, 0])
-    print(res)
 
-        
 # @lc code=end
 
